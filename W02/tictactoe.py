14088bb27f9fde34495ec551f23677dffd634072
vallabisa <u068354659@byui.edu>
2022-01-14 09:51:16 +0800

    updated with has_winner, draw, next_player and move

--- W02/tictactoe.py
+++ W02/tictactoe.py
@@ -24,7 +24,25 @@ def create_board():
         board.append(square+1)
     return board
 
+def has_winner(board):
+    if board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[0] == board[3] == board[6] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
+        return True
 
+def draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True
+
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+def move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
 
 
 
