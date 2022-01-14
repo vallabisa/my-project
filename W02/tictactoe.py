1f386a9674f42fc5f97414638843a6bba7caabb7
vallabisa <u068354659@byui.edu>
2022-01-14 09:32:59 +0800

    functions for creating board

--- W02/tictactoe.py
+++ W02/tictactoe.py
@@ -2,3 +2,31 @@
 # This program runs a tictactoe game
 #Author: Val Lorence Labisa
 
+
+def main():
+
+
+
+
+def print_board(board):
+    print()
+    print(f'{board[0]}|{board[1]}|{board[2]}')
+    print('-+-+-')
+    print(f'{board[3]}|{board[4]}|{board[5]}')
+    print('-+-+-')
+    print(f'{board[6]}|{board[7]}|{board[8]}')
+    print()
+
+def create_board():
+    board = []
+    square = 0
+    for square in range(9):
+        board.append(square+1)
+    return board
+
+
+
+
+
+if __name__ == "__main__":
+    main()
